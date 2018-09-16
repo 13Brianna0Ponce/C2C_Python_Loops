2282ab5f7f4b44cc6f374ae477ed78474ee1166a
mough <mmslavin@gmail.com>
2018-09-16 17:21:01 -0500

    Create index_practice.py

---
+++ index_practice.py
@@ -0,0 +1,6 @@
+# index_practice.py
+days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+print(days_of_the_week[1:4])
+print(days_of_the_week[0:-1])
+print(days_of_the_week[0:-1:2])
